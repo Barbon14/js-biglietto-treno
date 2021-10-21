2ed525ace3a17a5e32ba3c4e02af0444304c33f3
BarbonFisso <barbieri.mat@hotmail.it>
2021-10-21 15:20:19 +0200

    terminato esercizio

--- js/main.js
+++ js/main.js
@@ -5,14 +5,36 @@
 // va applicato uno sconto del 40 % per gli over 65.
 // L’output del prezzo finale va messo fuori in forma umana(con massimo due decimali, per indicare centesimi sul prezzo)
 
-// dichiarazione variabili
-
 // chiedo all'utente quanti chilometri vuole percorrere
+const km = prompt('Quanti chilometri devi percorrere?');
+console.log(km);
 
 // chiedo all'utente quanti anni ha
+const anni = prompt('Quanti anni hai?');
+console.log(anni);
 
 // calcolo la tariffa base del biglietto 
+const subPrezzo = km * 0.21;
+console.log(subPrezzo);
 
 // controllo se l'utente ha diritto ad uno sconto e nel caso lo applico
+let prezzo;
+if (anni < 18) {
+    prezzo = subPrezzo * 0.8
+}
+
+else if (anni > 65) {
+    prezzo = subPrezzo * 0.6
+} 
+
+else {
+    prezzo = subPrezzo
+}
+console.log(prezzo);
+
+// arrotondo il prezzo a 2 decimali
+let prezzoArrotondato = Math.round((prezzo + Number.EPSILON) * 100) / 100;
+console.log(prezzoArrotondato);
 
 // comunico all'utente il prezzo del suo biglietto 
+document.getElementById("prezzo").innerHTML = `Il prezzo del tuo biglietto è ${prezzoArrotondato} &euro;`;
