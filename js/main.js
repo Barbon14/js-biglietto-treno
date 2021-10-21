4023d0fb116d27bd9afd319c320ceb27b73459d3
BarbonFisso <barbieri.mat@hotmail.it>
2021-10-21 14:40:24 +0200

    aggiunto analisi logica dello script

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@
 // va applicato uno sconto del 20 % per i minorenni
 // va applicato uno sconto del 40 % per gli over 65.
 // L’output del prezzo finale va messo fuori in forma umana(con massimo due decimali, per indicare centesimi sul prezzo)
+
+// dichiarazione variabili
+
+// chiedo all'utente quanti chilometri vuole percorrere
+
+// chiedo all'utente quanti anni ha
+
+// calcolo la tariffa base del biglietto 
+
+// controllo se l'utente ha diritto ad uno sconto e nel caso lo applico
+
+// comunico all'utente il prezzo del suo biglietto 
